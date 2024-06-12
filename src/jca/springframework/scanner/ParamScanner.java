363737371f1470704d8849b99732c38251d57191
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-06-12 09:11:36 +0300

    scann the parameter

---
+++ src/jca/springframework/scanner/ParamScanner.java
@@ -0,0 +1,18 @@
+package jca.springframework.scanner;
+
+import java.lang.reflect.Parameter;
+
+import jca.springframework.annotations.Param;
+
+public class ParamScanner {
+    static public boolean isParameterParam(Parameter  parameter){
+        return parameter.isAnnotationPresent(Param.class);
+    }
+    static public Param getParameterParam(Parameter parameter){
+        Param paramResult = null;
+        if (isParameterParam(parameter)) {
+            paramResult = parameter.getAnnotation(Param.class);
+        }
+        return paramResult;
+    }
+}
