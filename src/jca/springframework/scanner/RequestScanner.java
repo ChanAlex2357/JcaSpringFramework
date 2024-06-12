45fec9eb371a0f3f8b695a0c519cda573d2fe450
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-06-12 09:14:22 +0300

    get the appropriate param name

--- src/jca/springframework/scanner/RequestScanner.java
+++ src/jca/springframework/scanner/RequestScanner.java
@@ -3,10 +3,22 @@ package jca.springframework.scanner;
 import java.lang.reflect.Parameter;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jca.springframework.annotations.Param;
 
 public class RequestScanner {
     public static String getParameterValue(Parameter parameter,HttpServletRequest request){
         String value = request.getParameter(parameter.getName());
+        if (value == null) {
+            try {
+                Param param = ParamScanner.getParameterParam(parameter);
+                value = param.name();
+            }
+            catch (Exception err) {
+                /*
+                 * Exception pour un param qui n'est pas implementer
+                */
+            }
+        }
         return value;
     }
 }
