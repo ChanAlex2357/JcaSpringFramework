5da5e19a392cd9b1dad9962db4d2d0d1a6f4c56b
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-06-21 04:48:13 +0300

    primitive cast

--- src/jca/springframework/scanner/ParamScanner.java
+++ src/jca/springframework/scanner/ParamScanner.java
@@ -5,6 +5,11 @@ import java.lang.reflect.Parameter;
 import jca.springframework.annotations.Param;
 
 public class ParamScanner {
+    public static final Class<?>[] PRIMITIVE_TYPES = new Class[]{
+        String.class,
+        int.class,
+        double.class
+    };
     static public boolean isParameterParam(Parameter  parameter){
         return parameter.isAnnotationPresent(Param.class);
     }
@@ -14,5 +19,18 @@ public class ParamScanner {
             paramResult = parameter.getAnnotation(Param.class);
         }
         return paramResult;
+    }
+
+    public static boolean isPrimitifType(Parameter parameter){
+        boolean result = false;
+        // Recuperer la class type du parametre de la fonction du controller 
+        Class<?> parameterType = parameter.getType();
+        for(Class<?> primitives : PRIMITIVE_TYPES){
+            if (parameterType.equals(primitives)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 }
--- src/jca/springframework/scanner/RequestScanner.java
+++ src/jca/springframework/scanner/RequestScanner.java
@@ -6,12 +6,21 @@ import jakarta.servlet.http.HttpServletRequest;
 import jca.springframework.annotations.Param;
 
 public class RequestScanner {
-    public static String getParameterValue(Parameter parameter,HttpServletRequest request){
-        String value = request.getParameter(parameter.getName());
-        if (value == null) {
+    public static Object getParameterValue(Parameter parameter,HttpServletRequest request){
+        Object value = null;
+        if (ParamScanner.isPrimitifType(parameter)) {
+            value = getPrmitiveParameterValue(parameter, request);
+        }
+        return value;
+    }
+    private static Object getPrmitiveParameterValue(Parameter parameter , HttpServletRequest request){
+        // Le resultat attendue
+        Object result = null;
+        String parameterValue = request.getParameter(parameter.getName());
+        if (parameterValue == null) {
             try {
                 Param param = ParamScanner.getParameterParam(parameter);
-                value = request.getParameter(param.name());
+                parameterValue = request.getParameter(param.name());
             }
             catch (Exception err) {
                 /*
@@ -19,6 +28,20 @@ public class RequestScanner {
                 */
             }
         }
-        return value;
+        // Recuperer la class type du parametre de la fonction du controller 
+        Class<?> parameterType = parameter.getType();
+        if (parameterType.equals(String.class)){
+            /// Pas de caste si c'est un string
+            result = parameterValue;
+        }
+        else if (parameterType.equals(int.class)) {
+            /// Integer cast
+            result = Integer.parseInt(parameterValue);
+        }
+        else if (parameterType.equals(double.class)) {
+            /// Double cast
+            result = Double.parseDouble(parameterValue);
+        }
+        return result; 
     }
 }
