72bbb46c96513259c535fcc25c67a007443fcf2a
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-06-29 19:27:18 +0300

    session convertion

--- src/jca/springframework/session/WebSessionParser.java
+++ src/jca/springframework/session/WebSessionParser.java
@@ -1,11 +1,16 @@
 package jca.springframework.session;
 
 import java.util.Enumeration;
+import java.util.Set;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 public class WebSessionParser {
-    WebSession HttpSessionToWebSession(HttpSession session){
+    static public WebSession HttpSessionToWebSession(HttpServletRequest request){
+        return HttpSessionToWebSession(request.getSession(true));
+    }
+    static public WebSession HttpSessionToWebSession(HttpSession session){
         WebSession webSession = new WebSession();
         // Transferer les <key,value> de la session vers  websession
         Enumeration<String> keys = session.getAttributeNames();
@@ -13,6 +18,24 @@ public class WebSessionParser {
             String key = keys.nextElement();
             webSession.getSessionValues().put(key, session.getAttribute(key));
         }
+        session.setAttribute(null, keys);
         return webSession;
+    }
+
+    static public void WebSessionToHttpSession(WebSession webSession , HttpServletRequest request) {
+        WebSessionToHttpSession(webSession, request.getSession());
+    }
+    static public void WebSessionToHttpSession(WebSession webSession , HttpSession session) {
+
+        Set<String> keys = webSession.getSessionValues().keySet();
+        for (String key : keys) {
+            Object webSessionValue = webSession.get(key);
+            if (webSessionValue == null) {
+                session.removeAttribute(key);
+                continue;
+            }
+            session.setAttribute(key, webSessionValue );
+        }
+            
     }
 }
