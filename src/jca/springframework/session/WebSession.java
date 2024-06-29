7df2b02a26668480111990e813f4395c25b2936c
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-06-29 18:05:10 +0300

    Creation of the session class

---
+++ src/jca/springframework/session/WebSession.java
@@ -0,0 +1,47 @@
+package jca.springframework.session;
+
+import java.util.HashMap;
+
+import jakarta.servlet.http.HttpSession;
+
+public class WebSession {
+    private HashMap<String,Object> sessionValues;
+
+    WebSession(){
+        setSessionValues(new HashMap<String,Object>());
+    }
+    WebSession(HashMap<String,Object> mapValues){
+        setSessionValues(mapValues);
+    }
+    WebSession(HttpSession session){
+    }
+
+    public HashMap<String, Object> getSessionValues() {
+        return sessionValues;
+    }
+
+    private void setSessionValues(HashMap<String, Object> sessionValues) {
+        this.sessionValues = sessionValues;
+    }
+
+// Fuctions
+
+    public void add(String key, Object value){
+
+    }
+    public void remove(String key){
+
+    }
+    public Object get(String key){
+        Object result = getSessionValues().get(key);
+            
+        if (result == null) {
+            // Exception pour un valeur qui n'existe pas
+        }
+        return result; 
+    }
+    public void update(String key , Object value){
+
+    }
+    
+}
---
+++ src/jca/springframework/session/WebSessionBuilder.java
@@ -0,0 +1,5 @@
+package jca.springframework.session;
+
+public class WebSessionBuilder {
+    
+}
