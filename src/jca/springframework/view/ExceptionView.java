5fee210b5d93524449599824327f752d569ddf05
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-10-14 15:10:28 +0300

    Define the exception and response status

--- src/jca/springframework/view/ExceptionView.java
+++ src/jca/springframework/view/ExceptionView.java
@@ -1,10 +1,15 @@
 package jca.springframework.view;
 
+import java.io.IOException;
 import java.util.List;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jca.springframework.exception.FrameworkException;
 
 public class ExceptionView extends StringView{
+    int statusCode = 500;
     public ExceptionView(FrameworkException exception){
         super(getExceptionContent(exception));
     }
@@ -22,5 +27,16 @@ public class ExceptionView extends StringView{
             message += getExceptionContent(exception);
         }
         return message;
+    }
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+    public int getStatusCode() {
+        return statusCode;
+    }
+    @Override
+    public void dispatch(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setStatus(getStatusCode());
+        super.dispatch(req, resp);
     }
 }
