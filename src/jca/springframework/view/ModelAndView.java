6fc96a6299b9e615cd5d00fc359ab0dff52756de
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-12-06 13:22:46 +0300

    implement error attribute

--- src/jca/springframework/view/ModelAndView.java
+++ src/jca/springframework/view/ModelAndView.java
@@ -7,8 +7,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class ModelAndView extends View{
+    private String error;
     public ModelAndView (String viewPath){
         super(viewPath);
+    }
+    public ModelAndView (String viewPath , String error){
+        super(viewPath);
+        setError(error);
     }
     @Override
     public void dispatch(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -16,5 +21,11 @@ public class ModelAndView extends View{
         setAttributs(req);
         /// Dispatch vers la view demander
         req.getRequestDispatcher( getViewPath() ).forward(req,resp);
+    }
+    public String getError() {
+        return error;
+    }
+    public void setError(String error) {
+        this.error = error;
     }
 }
