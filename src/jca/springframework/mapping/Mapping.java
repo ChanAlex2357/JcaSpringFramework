f7c20e4f01b42a650c3724ca61dc63b1aebf1bbb
ChanAlex2357 <jacqueschanalex@gmail.com>
2025-01-07 21:47:50 +0300

    fix bad import

--- src/jca/springframework/mapping/Mapping.java
+++ src/jca/springframework/mapping/Mapping.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.naming.AuthenticationException;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
